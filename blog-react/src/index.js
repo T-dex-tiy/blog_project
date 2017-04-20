df11f1ba37df63ab2bc1611f1770a823f21f5311
T-Dex <dex.m.mills@gmail.com>
2017-04-19 20:33:21 -0600

    Add basic JSX. Remove uneeded JS files

--- blog-react/src/index.js
+++ blog-react/src/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './styles/app.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header"></div>
+        <div className="app-wrapper">
+          <div className="app-nav"></div>
+          <div className="main-content"></div>
+            {this.props.children}
+        </div>
+        <div className="app-footer"></div>
+      </div>
+    )
+  }
+
+}
 
 ReactDOM.render(
   <App />,
