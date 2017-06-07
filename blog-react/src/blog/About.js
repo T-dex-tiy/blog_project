affa8605d5dc8bd6ae0cf389fd7e2aec9e48dbe4
T-Dex <dex.m.mills@gmail.com>
2017-06-07 15:04:58 -0600

    Add hero pic for about page

--- blog-react/src/blog/About.js
+++ blog-react/src/blog/About.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Herobanner from '../images/herobanner.jpg';
 
 class about extends Component{
   constructor(props){
     super(props)
-    this.state={
-
-    }
   }
 
   render(){
     return (
       <div>
-      <p>Throwing ideas into this area. <br>Small hero header, followed by finsihed projects with links to Heroku pages/github pages</br>
-       Time to figure this out and then on to Browser Router and pushing to my personal website</p>
+      <img className="Hero" src={Herobanner}></img>
       </div>
     )
   }
